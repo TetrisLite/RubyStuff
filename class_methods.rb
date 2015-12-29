7761c7b57e974f1bceddf77a7f90b2e2e2ae244f
Ade Oke <oke.ade@gmail.com>
2015-12-29 16:19:09 +0000

    added class two methods

---
+++ class_methods.rb
@@ -0,0 +1,26 @@
+class Chores
+  def self.throw_bin_away
+    "Take the rubbish and put it the in dustin"
+  end
+
+  def self.iron_clothes
+    "Place clothing on ironning board and press with iron"
+  end
+end
+
+puts Chores.throw_bin_away
+
+
+class Delivery
+  class << self
+    def one_day_delivery
+      "quickly to your door"
+    end
+
+    def normal_business_days
+      "Up to five working days"
+    end
+  end
+end
+
+puts Delivery.one_day_delivery
