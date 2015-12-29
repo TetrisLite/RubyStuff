dba4dfe9829c9d46a27f7f2ac81decca33ab9fdf
Ade Oke <oke.ade@gmail.com>
2015-12-29 01:12:20 +0000

    added select and each methods to code

--- environment_variables.rb
+++ environment_variables.rb
@@ -10,3 +10,15 @@ puts ENV["USER"]
 week_and_numbers = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", 100, 200, 300]
 
 # we going to work on each, select, reject, collect
+
+week_and_numbers.each  do |day_or_number|
+  puts day_or_number
+end
+
+
+result = week_and_numbers.select do |day_or_number|
+  day_or_number.kind_of?(String)
+end
+
+p result
+puts result.length == 7
