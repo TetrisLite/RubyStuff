ce3d584513e97ad636263cab6bdb467429862661
Ade Oke <oke.ade@gmail.com>
2015-12-29 00:33:07 +0000

    added some ENV code to the file

--- environment_variables.rb
+++ environment_variables.rb
@@ -1,2 +1,10 @@
-# creating a new environment variable
+# creating a new environment variable COUNTRY
 ENV["COUNTRY"] = "United Kingdom"
+puts ENV["COUNRTY"] == "United Kingdom" ? "You're in the UK" : "You're not in the UK"
+
+puts ENV['PATH']
+puts ENV["MACHINE"]
+puts ENV["USERS"]
+puts ENV["USER"]
+
+week_and_numbers = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", 100, 200, 300]
