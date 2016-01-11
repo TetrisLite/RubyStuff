c9eacf59d7fa99f18457a7ba731b23778ba197f5
Ade Oke <oke.ade@gmail.com>
2016-01-11 12:19:58 +0000

    added a gift game

---
+++ gift.rb
@@ -0,0 +1,24 @@
+print "What is the name of the gift giver?"
+name = gets.chomp
+
+print "What present did they give you?"
+present = gets.chomp
+
+print "How old were you on your birthday?"
+age = Integer(gets)
+
+print "What's your name?"
+your_name = gets.chomp
+puts
+puts
+puts "Dear #{name}"
+puts
+puts "Thanks for the #{present}."
+puts "I really like it! I can't believe"
+puts "that I am already #{age} years old."
+puts "Being #{age} feels just like #{age-1}"
+puts
+puts "Sincerely"
+puts
+puts
+puts your_name
